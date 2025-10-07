88fc153dee064490d8fb2a107856874b445a6075
Personwritep <89448434+personwritep@users.noreply.github.com>
2025-10-08 00:22:48 +0900

    Update Display_ALT_Editor.user.js

--- Display_ALT_Editor.user.js
+++ Display_ALT_Editor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        Display ALT Editor
+// @name        Display ALT Editor ⭐
 // @namespace        http://tampermonkey.net/
-// @version        0.3
+// @version        0.4
 // @description        編集画面内の記事画像にマウスホバーでALTを表示
 // @author        Ameba Blog User
 // @match        https://blog.ameba.jp/ucs/entry/srventry*
@@ -12,88 +12,161 @@
 // @grant        none
 // ==/UserScript==
 
-let target=document.getElementById('cke_1_contents'); // 監視 target
-if(target){
-    let monitor=new MutationObserver(main);
-    monitor.observe(target, {childList: true}); }
 
-main();
+
+let type=localStorage.getItem('DispALT_E'); // サムネイル処理タイプ 🔵
+if(!type){
+    type=0; }
+localStorage.setItem('DispALT_E', type); // 初期値をセット
+
+let style=
+    '<style class="dispalt_el">'+
+    '.spui-ToggleSwitch-input:checked~.spui-ToggleSwitch-visual { '+
+    'background: #ff6a59; }</style>'+
+    '<style class="dispalt_et">'+
+    '.spui-ToggleSwitch-input:checked~.spui-ToggleSwitch-visual { '+
+    'background: #1da1f2; }</style>';
+
+if(!document.querySelector('.dispalt_el')){
+    document.body.insertAdjacentHTML('beforeend', style); }
+
+type_set();
+
+
+
+let retry=0;
+let interval=setInterval(wait_target, 100);
+function wait_target(){
+    retry++;
+    if(retry>10){ // リトライ制限 10回 1sec
+        clearInterval(interval); }
+    let target=document.getElementById('cke_1_contents'); // 監視 target
+    if(target){
+        clearInterval(interval);
+        main(); }}
+
 
 function main(){
-    let editor_iframe=document.querySelector('.cke_wysiwyg_frame');
-    if(editor_iframe){ // iframe読込みが実行条件
-        let iframe_doc=editor_iframe.contentWindow.document;
-        if(iframe_doc){
 
-            let adisp=
-                '<div class="alt_disp"><span ></span>'+
-                '<style>'+
-                '.alt_disp { position: absolute; display: none; font: normal 14px/16px Meiryo; '+
-                'padding: 3px 6px 1px; color: #000; border: 1px solid #aaa; background: #fff; }'+
-                '</style></div>';
+    let target0=document.getElementById('cke_1_contents'); // 監視 target
+    if(target0){
+        let monitor0=new MutationObserver(main_in);
+        monitor0.observe(target0, {childList: true}); }
 
-            if(!iframe_doc.querySelector('.alt_disp')){
-                iframe_doc.documentElement.insertAdjacentHTML('beforeend', adisp); }
+    main_in();
 
+    function main_in(){
+        let editor_iframe=document.querySelector('.cke_wysiwyg_frame');
+        if(editor_iframe){ // iframe読込みが実行条件
+            let iframe_doc=editor_iframe.contentWindow.document;
+            if(iframe_doc){
+                let adisp=
+                    '<div class="alt_disp"><span ></span>'+
+                    '<style>'+
+                    '.alt_disp { position: absolute; z-index: 10; font: normal 14px/16px Meiryo; '+
+                    'padding: 3px 6px 1px; color: #000; border: 1px solid #aaa; background: #fff; '+
+                    'display: none; }'+
+                    '</style></div>';
 
-
-            let target0=iframe_doc.querySelector('body.cke_editable'); // 監視 target
-            if(target0){
-            let monitor0=new MutationObserver(img_check);
-            monitor0.observe(target0, {childList: true}); }
-
-            img_check();
-
-            function img_check(){
-                let all_img=iframe_doc.querySelectorAll('img');
-                for(let k=0; k<all_img.length; k++){
-                    all_img[k].addEventListener('mouseover', function(){
-                        disp(all_img[k]); }); }
-
-                // 引数「0」はサムネイル画像の「代替テキスト」に「🔗」を記入する　🔴
-                // 引数「1」はサムネイル画像の「代替テキスト」自動追加を有効にする 🔴
-                card_thumb(0);
-
-            } // img_check()
-
-
-            function disp(pelement){
-                let spos_y=iframe_doc.documentElement.scrollTop;
-                let pos_x=pelement.getBoundingClientRect().left+4;
-                let pos_y=pelement.getBoundingClientRect().top+spos_y+2;
-
-                let alt_text=pelement.getAttribute('alt');
-                let alt_disp=iframe_doc.querySelector('.alt_disp');
-                let alt_disp_span=iframe_doc.querySelector('.alt_disp span');
-                if(alt_text && alt_disp && alt_disp_span){
-                    alt_disp_span.textContent=alt_text;
-                    alt_disp.style.left=pos_x+'px';
-                    alt_disp.style.top=pos_y+'px';
-                    alt_disp.style.display='block';
-
-                    disp_out(pelement, alt_disp); }}
-
-
-            function disp_out(pelem, alt_disp){
-                pelem.onmouseleave=()=>{
-                    alt_disp.style.display='none'; }
-                pelem.onmouseover=()=>{
-                    alt_disp.style.display='block'; }
-                alt_disp.onmouseover=()=>{
-                    alt_disp.style.display='block'; }
-                alt_disp.onmouseleave=()=>{
-                    alt_disp.style.display='none'; }}
+                if(!iframe_doc.querySelector('.alt_disp')){
+                    iframe_doc.documentElement.insertAdjacentHTML('beforeend', adisp); }
 
 
 
-            function card_thumb(n){
-                let card_image=iframe_doc.querySelectorAll('.ogpCard_image');
-                for(let k=0; k<card_image.length; k++){
-                    if(n==0){
-                        card_image[k].setAttribute('alt', '🔗'); }
-                    else{
-                        if(card_image[k].getAttribute('alt')=='🔗'){
-                            card_image[k].setAttribute('alt', ''); }}}}
+                let target1=iframe_doc.querySelector('body.cke_editable'); // 監視 target
+                if(target1){
+                    let monitor1=new MutationObserver(img_check);
+                    monitor1.observe(target1, {childList: true}); }
 
-        }} // if(editor_iframe)
+                img_check();
+
+                function img_check(){
+                    let all_img=iframe_doc.querySelectorAll('img');
+                    for(let k=0; k<all_img.length; k++){
+                        all_img[k].addEventListener('mouseover', function(){
+                            disp(all_img[k]); }); }
+
+                    card_thumb();
+
+                } // img_check()
+
+
+                function disp(pelement){
+                    let spos_y=iframe_doc.documentElement.scrollTop;
+                    let pos_x=pelement.getBoundingClientRect().left+4;
+                    let pos_y=pelement.getBoundingClientRect().top+spos_y+2;
+
+                    let alt_text=pelement.getAttribute('alt');
+                    let alt_disp=iframe_doc.querySelector('.alt_disp');
+                    let alt_disp_span=iframe_doc.querySelector('.alt_disp span');
+                    if(alt_text && alt_disp && alt_disp_span){
+                        alt_disp_span.textContent=alt_text;
+                        alt_disp.style.left=pos_x+'px';
+                        alt_disp.style.top=pos_y+'px';
+                        alt_disp.style.display='block';
+
+                        disp_out(pelement, alt_disp); }}
+
+
+                function disp_out(pelem, alt_disp){
+                    pelem.onmouseleave=()=>{
+                        alt_disp.style.display='none'; }
+                    pelem.onmouseover=()=>{
+                        alt_disp.style.display='block'; }
+                    alt_disp.onmouseover=()=>{
+                        alt_disp.style.display='block'; }
+                    alt_disp.onmouseleave=()=>{
+                        alt_disp.style.display='none'; }}
+
+
+
+                function card_thumb(){
+                    let card_image=iframe_doc.querySelectorAll('.ogpCard_image');
+                    for(let k=0; k<card_image.length; k++){
+                        if(type==0){
+                            card_image[k].setAttribute('alt', '🔗'); }
+                        else{
+                            if(card_image[k].src.includes('.gif')){
+                                card_image[k].setAttribute('alt', '🔗'); }
+                            else{
+                                if(card_image[k].getAttribute('alt')=='🔗'){
+                                    card_image[k].setAttribute('alt', ''); }}}}
+
+                } //card_thumb()
+
+            }} // if(editor_iframe)
+
+    } // main_in()
+
 } // main()
+
+
+
+function type_set(){
+    let dp_el=document.querySelector('.dispalt_el');
+    let dp_et=document.querySelector('.dispalt_et');
+    if(dp_el && dp_et){
+        if(type==0){
+            dp_el.disabled=false;
+            dp_et.disabled=true; }
+        else{
+            dp_el.disabled=true;
+            dp_et.disabled=false; }
+
+        let alt_sw=document.querySelector('#js-aiAltFlag');
+        if(alt_sw){
+            alt_sw.onclick=(event)=>{
+                if(event.ctrlKey){
+                    event.preventDefault();
+                    if(type==0){
+                        type=1;
+                        dp_el.disabled=true;
+                        dp_et.disabled=false; }
+                    else{
+                        type=0;
+                        dp_el.disabled=false;
+                        dp_et.disabled=true; }
+
+                    localStorage.setItem('DispALT_E', type); }}}} // 🔵
+
+} // type_set()
